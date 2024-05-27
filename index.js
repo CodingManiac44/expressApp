89b4fec6e97d830a709c46c512e3a446cf0fbfa8
Md. Saidur Rahman <37741326+saidurNsu99@users.noreply.github.com>
2024-05-27 14:32:24 +0600

    about, home get methods

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get("/",function(req,res){
 res.send("hello express js");
 });
 
+app.get("/home",function(req,res){
+res.send("home page");
+});
+app.get("/about",function(req,res){
+res.send("about page");
+});
+
 app.listen(8000,function(){
 console.log("watever");
 })
