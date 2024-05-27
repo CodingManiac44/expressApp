8a055bfd36dfebfb92609fc215dceb83d3ddc728
Md. Saidur Rahman <37741326+saidurNsu99@users.noreply.github.com>
2024-05-27 16:25:36 +0600

    app.js added json response along with respone code for /unath endpoint

--- app.js
+++ app.js
@@ -4,8 +4,25 @@ app=express();
 app.get("/",function(req,res){
 res.send("hello express js");
 });
+app.get("/unath",function(req,res){
+res.status(401).end("unath");
+});
+
+app.get("/jsondata",function(req,res){
+  let myJsonArray=[
+    {
+      name:"sabbir",
+      city:"dhaka"
+    },
+    {
+      name:"sabbir",
+      city:"dhaka"
+    }
+  ]
+res.json(myJsonArray);
+});
 app.get("/one",function(req,res){
-res.send("get express js");
+    res.send("get js");
 });
 app.post("/two",function(req,res){
 res.send("post req home page");
@@ -27,6 +44,16 @@ res.send("patch page");
 app.get("/three",function(req,res){
 res.send("get req home page");
 });
+app.get("/threeRes",function(req,res){
+
+    res.end("this is a string");
+
+});
+
+// app.get("/threeCode",function(req,res){
+//     res.status(401).end("Unathorized");
+// });
+
 
 app.listen(9000,function(){
 console.log("watever");
